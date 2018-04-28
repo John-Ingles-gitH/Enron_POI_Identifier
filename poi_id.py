cdc8a19c9a0fe094b5989c9d6bd8980a2ac89700
John Ingles <john.kenneth.ingles@gmail.com>
2018-04-27 22:09:44 -0400

    feat: Add basic Decision Tree classifier using features selected by SelectKBest
    Added code to load final_project_dataset.pkl.  Removed an outlier from the set.
    Used cross_validation to create a training testing split of the data.
    Used SelectKBest to select 4 features from a features list.
    Trained a DecisionTreeClassifier and printed its accuracy, precision,
    and recall when used with the testing data.

--- poi_id.py
+++ poi_id.py
@@ -2,6 +2,9 @@
 
 import sys
 import pickle
+import numpy as np
+import matplotlib.pyplot as plt
+
 sys.path.append("../tools/")
 
 from feature_format import featureFormat, targetFeatureSplit
@@ -10,7 +13,10 @@ from tester import dump_classifier_and_data
 ### Task 1: Select what features you'll use.
 ### features_list is a list of strings, each of which is a feature name.
 ### The first feature must be "poi".
-features_list = ['poi','salary'] # You will need to use more features
+
+features_list = ["poi",'salary', 'deferral_payments', 'total_payments', 'bonus', 'deferred_income', 
+                 'total_stock_value', 'expenses', 'exercised_stock_options', 'other', 
+                 'long_term_incentive', 'restricted_stock']
 
 ### Load the dictionary containing the dataset
 with open("final_project_dataset.pkl", "r") as data_file:
@@ -26,7 +32,6 @@ my_dataset = data_dict
 ### Extract features and labels from dataset for local testing
 data = featureFormat(my_dataset, features_list, sort_keys = True)
 labels, features = targetFeatureSplit(data)
-print data
 
 ### Task 4: Try a varity of classifiers
 ### Please name your classifier clf for easy export below.
@@ -35,8 +40,53 @@ print data
 ### http://scikit-learn.org/stable/modules/pipeline.html
 
 # Provided to give you a starting point. Try a variety of classifiers.
-from sklearn.naive_bayes import GaussianNB
-clf = GaussianNB()
+from sklearn import cross_validation
+from sklearn.feature_selection import SelectKBest, f_classif
+from time import time
+from sklearn import tree
+from sklearn.metrics import accuracy_score, precision_score, recall_score
+
+#lists for storing individual model accuracies, precisions, and recalls
+acc_list=[]
+prec_list=[]
+rec_list=[]
+
+#loop for running models with different random_state for each
+for n in range(1,100):
+    #create training/testing split
+    features_train, features_test, labels_train, labels_test = cross_validation.train_test_split(features, labels, test_size=0.1, random_state=n)
+    
+    #features selected
+    selector = SelectKBest(f_classif, k=4)
+    selector.fit(features_train, labels_train)
+    
+    #transformed
+    features_train = selector.transform(features_train)
+    features_test = selector.transform(features_test)
+    
+    #Decision Tree used as classifier
+    clf=tree.DecisionTreeClassifier()
+    
+    #Fit to training data
+    clf.fit(features_train,labels_train)
+    
+    #predictions made using test data
+    pred = clf.predict(features_test)
+    
+    #accuracy, precision, and recall calculated
+    acc = accuracy_score(pred, labels_test)
+    prec = precision_score(pred, labels_test)
+    recall = recall_score(pred, labels_test)
+    
+    acc_list.append(acc)
+    prec_list.append(prec)
+    rec_list.append(recall)
+
+#the mean accuracy, precision and recall of the 100 models is used in since the number of data points is low and
+#the chance of test sets with 0 poi is not trivial.
+print np.mean(acc_list)
+print np.mean(prec_list)
+print np.mean(rec_list)
 
 ### Task 5: Tune your classifier to achieve better than .3 precision and recall 
 ### using our testing script. Check the tester.py script in the final project
