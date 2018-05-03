05e71dc13fe851e94220032e3f653ce65682c032
John Ingles <john.kenneth.ingles@gmail.com>
2018-05-02 20:52:21 -0400

    feat: Add function test_and_train to see output of tester.py in advance
    test_and_train function produces output that tester.py will.  In addition,
    it output's features scores of SelectKBest, the features selected, and
    the accuracy, precision, and recall of the training data (to test that it
    the feature bias isn't too high)

--- poi_id.py
+++ poi_id.py
@@ -9,6 +9,7 @@ sys.path.append("../tools/")
 
 from feature_format import featureFormat, targetFeatureSplit
 from tester import dump_classifier_and_data
+from sklearn.cross_validation import StratifiedShuffleSplit
 
 ### Task 1: Select what features you'll use.
 ### features_list is a list of strings, each of which is a feature name.
@@ -16,7 +17,99 @@ from tester import dump_classifier_and_data
 
 features_list = ["poi",'salary', 'deferral_payments', 'total_payments', 'bonus', 'deferred_income', 
                  'total_stock_value', 'expenses', 'exercised_stock_options', 'other', 
-                 'long_term_incentive', 'restricted_stock']
+                 'long_term_incentive', 'restricted_stock','to_messages','from_poi_to_this_person',
+                 'from_messages', 'from_this_person_to_poi', 'shared_receipt_with_poi']
+
+PERF_FORMAT_STRING = "\
+\tAccuracy: {:>0.{display_precision}f}\tPrecision: {:>0.{display_precision}f}\t\
+Recall: {:>0.{display_precision}f}\tF1: {:>0.{display_precision}f}\tF2: {:>0.{display_precision}f}"
+RESULTS_FORMAT_STRING = "\tTotal predictions: {:4d}\tTrue positives: {:4d}\tFalse positives: {:4d}\
+\tFalse negatives: {:4d}\tTrue negatives: {:4d}"
+
+def train_and_test(clf, dataset, feature_list, folds = 1000):
+    data = featureFormat(dataset, feature_list, sort_keys = True)
+    labels, features = targetFeatureSplit(data)
+    cv = StratifiedShuffleSplit(labels, folds, random_state = 42)
+    true_negatives = 0
+    false_negatives = 0
+    true_positives = 0
+    false_positives = 0
+    true_negatives_train = 0
+    false_negatives_train = 0
+    true_positives_train = 0
+    false_positives_train = 0
+    for train_idx, test_idx in cv: 
+        features_train = []
+        features_test  = []
+        labels_train   = []
+        labels_test    = []
+        for ii in train_idx:
+            features_train.append( features[ii] )
+            labels_train.append( labels[ii] )
+        for jj in test_idx:
+            features_test.append( features[jj] )
+            labels_test.append( labels[jj] )
+        
+        ### fit the classifier using training set, and test on test set
+        clf.fit(features_train, labels_train)
+        predictions = clf.predict(features_test)
+        training_predicitons = clf.predict(features_train)
+        for prediction, truth in zip(predictions, labels_test):
+            if prediction == 0 and truth == 0:
+                true_negatives += 1
+            elif prediction == 0 and truth == 1:
+                false_negatives += 1
+            elif prediction == 1 and truth == 0:
+                false_positives += 1
+            elif prediction == 1 and truth == 1:
+                true_positives += 1
+            else:
+                print "Warning: Found a predicted label not == 0 or 1."
+                print "All predictions should take value 0 or 1."
+                print "Evaluating performance for processed predictions:"
+                break
+        for prediction, truth in zip(training_predicitons, labels_train):
+            if prediction == 0 and truth == 0:
+                true_negatives_train += 1
+            elif prediction == 0 and truth == 1:
+                false_negatives_train += 1
+            elif prediction == 1 and truth == 0:
+                false_positives_train += 1
+            elif prediction == 1 and truth == 1:
+                true_positives_train += 1
+            else:
+                print "Warning: Found a predicted label not == 0 or 1."
+                print "All predictions should take value 0 or 1."
+                print "Evaluating performance for processed predictions:"
+                break
+    try:
+        total_predictions = true_negatives + false_negatives + false_positives + true_positives
+        accuracy = 1.0*(true_positives + true_negatives)/total_predictions
+        precision = 1.0*true_positives/(true_positives+false_positives)
+        recall = 1.0*true_positives/(true_positives+false_negatives)
+        f1 = 2.0 * true_positives/(2*true_positives + false_positives+false_negatives)
+        f2 = (1+2.0*2.0) * precision*recall/(4*precision + recall)
+        print clf
+        print PERF_FORMAT_STRING.format(accuracy, precision, recall, f1, f2, display_precision = 5)
+        print RESULTS_FORMAT_STRING.format(total_predictions, true_positives, false_positives, false_negatives, true_negatives)
+        print ""
+    except:
+        print "Got a divide by zero when trying out:", clf
+        print "Precision or recall may be undefined due to a lack of true positive predicitons."
+
+    try:
+        total_predictions_train = true_negatives_train + false_negatives_train + false_positives_train + true_positives_train
+        accuracy_train = 1.0*(true_positives_train + true_negatives_train)/total_predictions_train
+        precision_train = 1.0*true_positives_train/(true_positives_train+false_positives_train)
+        recall_train = 1.0*true_positives_train/(true_positives_train+false_negatives_train)
+        f1_train = 2.0 * true_positives_train/(2*true_positives_train + false_positives_train+false_negatives_train)
+        f2_train = (1+2.0*2.0) * precision_train*recall_train/(4*precision_train + recall_train)
+        print PERF_FORMAT_STRING.format(accuracy_train, precision_train, recall_train, f1_train, f2_train, display_precision = 5)
+        print RESULTS_FORMAT_STRING.format(total_predictions_train, true_positives_train, false_positives_train, false_negatives_train, true_negatives_train)
+        print ""
+    except:
+        print "Got a divide by zero when trying out:", clf
+        print "Precision or recall may be undefined due to a lack of true positive predicitons."			 
 
 ### Load the dictionary containing the dataset
 with open("final_project_dataset.pkl", "r") as data_file:
@@ -51,14 +144,16 @@ from sklearn.metrics import accuracy_score, precision_score, recall_score
 #features_train, features_test, labels_train, labels_test = cross_validation.train_test_split(features, labels, test_size=0.1, random_state=1)
 
 #features selected
-selector = SelectKBest(f_classif, k=4)
+selector = SelectKBest(f_classif, k=3)
 selector.fit(features,labels)
+print selector.scores_
 #selector.fit(features_train, labels_train)
 
 #Drop features not selected from features_list
 features_list_no_poi = features_list[1:]
 features_list = [i for (i, v) in zip(features_list_no_poi, selector.get_support()) if v]
 features_list.insert(0, "poi")
+print features_list
 #transformed
 #features_train = selector.transform(features_train)
 #features_test = selector.transform(features_test)
@@ -70,11 +165,11 @@ scoring = {'Accuracy':make_scorer(accuracy_score),'Precision':make_scorer(precis
 #parameters = {'max_depth':[1,2,3,4,5],'min_samples_split':[2,3,4,5]}
 #dt = tree.DecisionTreeClassifier(random_state=1)
 #clf = GridSearchCV(dt, parameters,scoring=scoring,refit='Accuracy')
-clf = GaussianNB()
+#clf = GaussianNB()
   
 #Decision Tree used as classifier
-#clf=tree.DecisionTreeClassifier(max_depth=5, random_state=1)
-
+clf=tree.DecisionTreeClassifier(random_state=1)
+train_and_test(clf,my_dataset,features_list)
 #Fit to training data
 #clf.fit(features_train,labels_train)
 #print(clf.best_params_)
