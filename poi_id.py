48332e8e261822285416aca91b29b194b53b1fc0
John Ingles <john.kenneth.ingles@gmail.com>
2018-06-27 21:53:24 -0400

    feat: Replace poi_id.py with final version, Add outfiles and writeup

--- poi_id.py
+++ poi_id.py
@@ -1,96 +1,55 @@
 #!/usr/bin/python
 
-import sys
 import pickle
 import numpy as np
 
-sys.path.append("../tools/")
-
 from feature_format import featureFormat, targetFeatureSplit
-from tester import dump_classifier_and_data
+from tester import dump_classifier_and_data, test_classifier
+from sklearn.feature_selection import SelectKBest
+from sklearn.pipeline import Pipeline
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.linear_model import LogisticRegression
+from sklearn.grid_search import GridSearchCV
+from sklearn.cross_validation import StratifiedShuffleSplit, train_test_split
+from sklearn.preprocessing import MinMaxScaler
 
 features_list = ['poi','salary', 'deferral_payments', 'total_payments', 'loan_advances', 'bonus',
                  'restricted_stock_deferred', 'deferred_income', 'total_stock_value', 'expenses',
-                 'exercised_stock_options', 'other', 'long_term_incentive', 'restricted_stock', 
+                 'exercised_stock_options', 'long_term_incentive', 'restricted_stock', 
                  'director_fees','to_messages','from_poi_to_this_person', 'from_messages',
                  'from_this_person_to_poi', 'shared_receipt_with_poi'] 
 
-
-
-def train_and_test(clf, dataset, feature_list, folds = 1000):
-    
-    PERF_FORMAT_STRING = "\
-    \tAccuracy: {:>0.{display_precision}f}\tPrecision: {:>0.{display_precision}f}\t\
-    Recall: {:>0.{display_precision}f}\tF1: {:>0.{display_precision}f}\tF2: {:>0.{display_precision}f}"
-    RESULTS_FORMAT_STRING = "\tTotal predictions: {:4d}\tTrue positives: {:4d}\tFalse positives: {:4d}\
-    \tFalse negatives: {:4d}\tTrue negatives: {:4d}"
-    
-    from sklearn.cross_validation import StratifiedShuffleSplit
-    
-    data = featureFormat(dataset, feature_list, sort_keys = True)
-    labels, features = targetFeatureSplit(data)
-    
-    cv = StratifiedShuffleSplit(labels, folds, random_state = 42)
-    
-    true_negatives = 0
-    false_negatives = 0
-    true_positives = 0
-    false_positives = 0
-    
-    for train_idx, test_idx in cv: 
-        features_train = []
-        features_test  = []
-        labels_train   = []
-        labels_test    = []
-        for ii in train_idx:
-            features_train.append( features[ii] )
-            labels_train.append( labels[ii] )
-        for jj in test_idx:
-            features_test.append( features[jj] )
-            labels_test.append( labels[jj] )
-        
-        ### fit the classifier using training set, and test on test set
-        clf.fit(features_train, labels_train)
-        predictions = clf.predict(features_test)
-        for prediction, truth in zip(predictions, labels_test):
-            if prediction == 0 and truth == 0:
-                true_negatives += 1
-            elif prediction == 0 and truth == 1:
-                false_negatives += 1
-            elif prediction == 1 and truth == 0:
-                false_positives += 1
-            elif prediction == 1 and truth == 1:
-                true_positives += 1
-            else:
-                print "Warning: Found a predicted label not == 0 or 1."
-                print "All predictions should take value 0 or 1."
-                print "Evaluating performance for processed predictions:"
-                break
-    try:
-        total_predictions = true_negatives + false_negatives + false_positives + true_positives
-        accuracy = 1.0*(true_positives + true_negatives)/total_predictions
-        precision = 1.0*true_positives/(true_positives+false_positives)
-        recall = 1.0*true_positives/(true_positives+false_negatives)
-        f1 = 2.0 * true_positives/(2*true_positives + false_positives+false_negatives)
-        f2 = (1+2.0*2.0) * precision*recall/(4*precision + recall)
-        print "\n"
-        print clf
-        print "\n"
-        print PERF_FORMAT_STRING.format(accuracy, precision, recall, f1, f2, display_precision = 5)
-        print RESULTS_FORMAT_STRING.format(total_predictions, true_positives, false_positives, false_negatives, true_negatives)
-        print ""
-    except:
-        print "Got a divide by zero when trying out:", clf
-        print "Precision or recall may be undefined due to a lack of true positive predicitons."
 
 ### Load the dictionary containing the dataset
 with open("final_project_dataset.pkl", "r") as data_file:
     data_dict = pickle.load(data_file)
 
-### Remove outlier
+### Remove outliers
 data_dict.pop("TOTAL",0)
+data_dict.pop('THE TRAVEL AGENCY IN THE PARK')
 
-### Task 3: Create new feature(s)
+### Create new features
+employee_names = data_dict.keys()
+
+for person in employee_names:
+    
+    messages_to_this_person = float(data_dict[person]['to_messages'])
+    messages_from_this_person = float(data_dict[person]['from_messages'])
+    messages_sent_to_poi = float(data_dict[person]['from_this_person_to_poi'])
+    messages_received_from_poi = float(data_dict[person]['from_poi_to_this_person'])
+
+    if messages_from_this_person > 0:
+        data_dict[person]['fraction_sent_to_poi'] = messages_sent_to_poi / messages_from_this_person
+    else:
+        data_dict[person]['fraction_sent_to_poi'] = 0
+
+    if messages_to_this_person > 0:
+        data_dict[person]['fraction_received_from_poi'] = messages_received_from_poi / messages_to_this_person
+    else:
+        data_dict[person]['fraction_received_from_poi'] = 0
+
+features_list.append('fraction_sent_to_poi')
+features_list.append('fraction_received_from_poi')
 
 ### Store to my_dataset for easy export below.
 my_dataset = data_dict
@@ -105,37 +64,76 @@ labels, features = targetFeatureSplit(data)
 ### you'll need to use Pipelines. For more info:
 ### http://scikit-learn.org/stable/modules/pipeline.html
 
+features_train, features_test, labels_train, labels_test = train_test_split(features, labels, train_size=.90, stratify=labels, random_state=42)
+
 #features selected
-from sklearn.feature_selection import SelectKBest, f_classif
-selector = SelectKBest(f_classif, k=3)
-selector.fit(features,labels)
-print "\n"
-print "Selector Scores"
-print selector.scores_
+nan_counts = {}
+for feature in features_list:
+    for k,v in my_dataset.iteritems():
+	    if v[feature] != 'NaN':
+		    nan_counts[feature]=nan_counts.get(feature,0)+1
 
-#Drop features not selected from features_list and print selected features
-features_list_no_poi = features_list[1:]
-features_list = [i for (i, v) in zip(features_list_no_poi, selector.get_support()) if v]
-features_list.insert(0, "poi")
+number_of_features=10
+selector = SelectKBest(k=number_of_features)
+selector_transformed = selector.fit_transform(features_train,labels_train)
+indices = selector.get_support(True)
+
+features_selected_list = ['poi']
 print "\n"
-print "Features Selected"
-print features_list
-print "\n"
- 
+for index in indices:
+    print '%-25s Score: %-15f Non-nan: %d' % (features_list[index + 1], selector.scores_[index], nan_counts[features_list[index + 1]])
+    features_selected_list.append(features_list[index + 1])
+print "\n"	
+features_list = features_selected_list
 #Construct Classifier
-from sklearn import tree
-from sklearn.naive_bayes import GaussianNB
 
-clf=tree.DecisionTreeClassifier(random_state=1)
-print "DecisionTreeClassifier"
-print "______________________"
-train_and_test(clf,my_dataset,features_list)
+#redefine in term of updated features list
+data = featureFormat(my_dataset, features_list, sort_keys=True)
+labels, features = targetFeatureSplit(data)
+
+# Using stratified shuffle split cross validation because of the small size of the dataset
+shuffle_split = StratifiedShuffleSplit(labels, 1000, random_state=55)
+
+# Build pipeline
+estimators = []
+estimators.append(('minmax_scaler', MinMaxScaler()))
+
+#choose method
+estimators.append(('decision_tree', DecisionTreeClassifier()))
+#estimators.append(('logistic_reg', LogisticRegression()))
+
+model = Pipeline(estimators)
+
+#parameters for logistic regression
+parameters_lr = dict(logistic_reg__class_weight=['balanced'],
+                      logistic_reg__solver=['liblinear'],
+                      logistic_reg__C=range(1, 5),
+                      logistic_reg__random_state=range(52,54))
+
+#parameters for decision tree
+parameters_dt = dict(decision_tree__min_samples_leaf=range(1, 5),
+                     decision_tree__max_depth=range(1, 5),
+                     decision_tree__class_weight=['balanced'],
+                     decision_tree__criterion=['gini', 'entropy'])
+
+					 
+#Use GridSearchCV to test different classifiers and parameters
+#I tested logistic_reg and decision_tree
+
+cv = GridSearchCV(model, param_grid=parameters_dt, scoring='f1', cv=shuffle_split)
+
+cv.fit(features, labels)
 print "\n"
-clf=GaussianNB()
-print "Gaussian Naive Bayes"
-print "____________________"
-train_and_test(clf,my_dataset,features_list)
+print cv.best_params_
+print "\n"
+clf=cv.best_estimator_
 
+#clf = DecisionTreeClassifier(min_samples_leaf=2,
+                     #max_depth=2,
+                     #class_weight='balanced',
+                     #criterion='entropy')
+
+test_classifier(clf, my_dataset, features_list)
 ### Task 6: Dump your classifier, dataset, and features_list so anyone can
 ### check your results. You do not need to change anything below, but make sure
 ### that the version of poi_id.py that you submit can be run on its own and
